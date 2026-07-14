3fe085fc20ff80695256ff6d5d6b60359e6d8f65
Killana Manjusree <killanamanjusree@gmail.com>
2026-07-14 22:12:14 +0530

    new

--- data/reader.py
+++ data/reader.py
@@ -29,11 +29,11 @@ with open("data/anime.csv", "r") as file:
         "Romantic Comedy",
         "Isekai Fantasy"
     ]
-    option = input('enter the number 1 for anime name and 2 for anime genre   ')
+    option = input('enter the number 1 for anime name and 2 for anime genre and 3 for exit ')
     if option == "1":
        user_name = input('enter the name of the anime' )
        for row in reader:
-           if row['Name'].Lower() == user_name.Lower():
+           if row['Name'].lower() == user_name.lower():
                print(f"\n'anime name is {row['Name']}, genre is {row['Genre']}")
 
     elif option == "2":
@@ -46,6 +46,10 @@ with open("data/anime.csv", "r") as file:
         for row in reader:
                if row["Genre"].lower() == user_genre.lower():
                  print(row["Name"])
-            
+    elif option == "3":
+        print('Thank you for using the anim rec, Happy watching.')
+    else:
+        print('invalid option, please enter 1 or 2 or 3')
+        
 
        
