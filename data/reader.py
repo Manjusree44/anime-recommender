3dc100126d64a640482f7bd2fb2b2fd1bb3b5dbd
Killana Manjusree <killanamanjusree@gmail.com>
2026-07-21 13:38:55 +0530

    new1

--- data/reader.py
+++ data/reader.py
@@ -1,8 +1,7 @@
 import csv
 
-
 with open("data/anime.csv", "r") as file:
-    reader = csv.DictReader(file)
+    reader = list(csv.DictReader(file))
 
     genres = [
         "Psychological Thriller",
@@ -29,25 +28,43 @@ with open("data/anime.csv", "r") as file:
         "Romantic Comedy",
         "Isekai Fantasy"
     ]
-    option = input('enter the number 1 for anime name and 2 for anime genre and 3 for exit ')
+
+    option = input(
+    "1. Search by anime name\n"
+    "2. Search by genre\n"
+    "3. Exit\n\n"
+    "Enter your choice: "
+)
     if option == "1":
-       user_name = input('enter the name of the anime' )
-       for row in reader:
-           if row['Name'].lower() == user_name.lower():
-               print(f"\n'anime name is {row['Name']}, genre is {row['Genre']}")
+        user_name = input('enter the name of the anime ')
+        found = False
+        for row in reader:
+            if row['Name'].lower() == user_name.lower():
+                found = True
+
+                print("\nanime found\n")
+                print(f"🎬 Anime Name : {row['Name']}")
+                print(f"📂 Genre      : {row['Genre']}")
+
+        if not found:
+            print("\nanime not found\n")
 
     elif option == "2":
-        
+
         print("\nAvailable Genres:\n")
         for genre in genres:
-           print(f"- {genre}")
-        user_genre = input('enter the genre you want from the available genres ')   
+            print(f"- {genre}")
+
+        user_genre = input('enter the genre you want from the available genres ')
         print("\nRecommended Anime:(happy watching)\n")
+
         for row in reader:
-               if row["Genre"].lower() == user_genre.lower():
-                 print(row["Name"])
+            if row["Genre"].lower() == user_genre.lower():
+                print(row["Name"])
+
     elif option == "3":
         print('Thank you for using the anim rec, Happy watching.')
+
     else:
         print('invalid option, please enter 1 or 2 or 3')
         
